87e4a17f496c09560a3249f5a7fdb67d011b0430
Your Name <hayri.durmaz@tedu.edu.tr>
2020-07-19 20:38:33 +0300

    Updates

---
+++ precache-manifest.5ab296f3dba21e242c192ed8c28c41fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3413f3c4fa828a63a7fec326e6eb2049",
+    "url": "/hayridurmaz/index.html"
+  },
+  {
+    "revision": "93f26252d4b0f7fa0fd2",
+    "url": "/hayridurmaz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8cc9bb3c9eb67d856152",
+    "url": "/hayridurmaz/static/js/2.0000deec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hayridurmaz/static/js/2.0000deec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f26252d4b0f7fa0fd2",
+    "url": "/hayridurmaz/static/js/main.00f26b9a.chunk.js"
+  },
+  {
+    "revision": "733bb509e05ca41c79e8",
+    "url": "/hayridurmaz/static/js/runtime-main.0f22034a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hayridurmaz/static/media/logo.5d5d9eef.svg"
+  }
+]);
