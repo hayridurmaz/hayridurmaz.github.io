8e9243cef155d2477f54efd3a5538c8861c4771d
Hayri Durmaz <hayridurmaz52@gmail.com>
2022-07-11 17:53:55 +0300

    Updates

---
+++ precache-manifest.2b00ac10d8a42ccc50f1e6728e72aca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183a236e115c689630e8eb072dabdf2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735db6b85eac0a19a44c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0bd44492b065a198723b",
+    "url": "/static/js/2.3fcad296.chunk.js"
+  },
+  {
+    "revision": "2134ff88658796fd5ce0ed90d44d9a7d",
+    "url": "/static/js/2.3fcad296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735db6b85eac0a19a44c",
+    "url": "/static/js/main.6d0129f9.chunk.js"
+  },
+  {
+    "revision": "2b0dd272850af8e25de4",
+    "url": "/static/js/runtime-main.d585db3a.js"
+  }
+]);
