84a1b1e2d820ae6b7f99fb992bc69fa15601b957
Hayri Durmaz <hayridurmaz52@gmail.com>
2023-11-23 22:59:52 +0300

    Updates

--- precache-manifest.136a2a14a3c05d2f0d909d74396de790.js
+++ precache-manifest.136a2a14a3c05d2f0d909d74396de790.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "183a236e115c689630e8eb072dabdf2d",
+    "revision": "c7e1b9a515d351b6160cd64940580460",
     "url": "/index.html"
   },
   {
-    "revision": "735db6b85eac0a19a44c",
+    "revision": "64a176a6cc6ced0444d8",
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3fcad296.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "735db6b85eac0a19a44c",
-    "url": "/static/js/main.6d0129f9.chunk.js"
+    "revision": "64a176a6cc6ced0444d8",
+    "url": "/static/js/main.14597c6c.chunk.js"
   },
   {
     "revision": "2b0dd272850af8e25de4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b00ac10d8a42ccc50f1e6728e72aca3.js"
+  "/precache-manifest.136a2a14a3c05d2f0d909d74396de790.js"
 );
 
 self.addEventListener('message', (event) => {
