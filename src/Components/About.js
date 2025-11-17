35c0a6412061db3b1494c376a660f76284ba948b
Hayri Durmaz <hayridurmaz52@gmail.com>
2025-11-17 14:34:08 +0300

    feature: Modernized the UI

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,6 @@ class About extends Component {
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
@@ -48,7 +47,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} className="button">
+                  <a href={resumeDownload} className="button" target="_blank" rel="noreferrer">
                     <i className="fa fa-download"></i>Download Resume
                   </a>
                 </p>
